fb080693a830184516b72d143e3d66560297adcf
Gustavo Silva <gustavosilvafranco643@gmail.com>
2021-06-09 13:26:41 -0300

    feat: Routes imported in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import GlobalStyle from "./styles/global";
 import { ToastContainer } from "react-toastify";
+import Routes from "./routes";
 
 function App() {
   return (
     <>
       <ToastContainer />
       <GlobalStyle />
+      <Routes />
     </>
   );
 }

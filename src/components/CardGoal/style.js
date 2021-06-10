4b0957f468004e49ca9ed646b68c306244ad6077
Gustavo Silva <gustavosilvafranco643@gmail.com>
2021-06-10 21:44:46 +0000

    feat:CardGoal and CardActivie components createds and integration with the api...

---
+++ src/components/CardGoal/style.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const GoalDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    font-size: 24px;
+    border: 1px solid black;
+    border-radius: 10px;
+    padding: 15px;
+    width: 90%;
+    margin: 10px;
+
+    p {
+        margin: 5px;
+    }
+    
+    span {
+        font-weight: bold;
+    }
+
+`
--- src/pages/SpecificGroup/index.jsx
+++ src/pages/SpecificGroup/index.jsx
@@ -22,14 +22,22 @@ import {
 import { IoMdPlay } from "react-icons/io";
 import Button from "../../components/Button"
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import api from "../../services/api";
+import CardGoal from "../../components/CardGoal";
+import CardActivitie from "../../components/CardActivitie";
 
 const SpecificGroup = () => {
 
+    const { id } = useParams()
+    
     const [goalsDivOpened, setGoalsDivOpened] = useState(false);
     const [activitiesDivOpened, setActivitiesDivOpened] = useState(false);
     const [isOpened, setIsOpened] = useState(true);
     const [goalsModalOpened, setGoalsModalOpened] = useState(false);
     const [activitiesModalOpened, setActivitiesModalOpened] = useState(false);
+
+    const [groupInfo, setGroupInfo] = useState([]);
 
     useEffect(() => {
         if (goalsModalOpened === true){
@@ -44,14 +52,21 @@ const SpecificGroup = () => {
 
     }, [goalsModalOpened,activitiesModalOpened,isOpened]);
 
+    useEffect(() => {
+        api.get(`/groups/${id}/`)
+        .then((group) => setGroupInfo(group.data))
+    }, [])
+
+    const { goals, activities } = groupInfo;
+
     return (
         <>
             <Header />
             <SpecificGroupMain>
                 <SpecificGroupDiv isOpened={isOpened}>
-                    <SpecificGroupH3>nome do grupo</SpecificGroupH3>
-                    <SpecificGroupP>category</SpecificGroupP>
-                    <SpecificGroupP>description</SpecificGroupP>
+                    <SpecificGroupH3>{groupInfo.name}</SpecificGroupH3>
+                    <SpecificGroupP>{groupInfo.category}</SpecificGroupP>
+                    <SpecificGroupP>{groupInfo.description}</SpecificGroupP>
                     <Container>
                         <GoalsDiv>
                             <GoalsButton onClick={() => setGoalsDivOpened(!goalsDivOpened)}> 
@@ -67,7 +82,7 @@ const SpecificGroup = () => {
                             </GoalsButton>
                             <GoalsNameDiv>Goals</GoalsNameDiv>
                             <GoalsListDiv goalsDivOpened={goalsDivOpened}>
-
+                                {goals && goals.map((goal) => <CardGoal goal={goal} key={goal.id}/>)}
                             </GoalsListDiv>
                             <SpaceDiv></SpaceDiv>
                             <Button widthMobile="300" func={() => setGoalsModalOpened(true)}> 
@@ -88,7 +103,7 @@ const SpecificGroup = () => {
                             </ActivitiesButton>
                             <ActivitiesNameDiv>Activities</ActivitiesNameDiv>
                             <ActivitiesListDiv activitiesDivOpened={activitiesDivOpened}>
-
+                                {activities && activities.map((activitie) => <CardActivitie activitie={activitie} key={activitie.id}/>)}
                             </ActivitiesListDiv>
                             <SpaceDiv> </SpaceDiv>
                             <Button widthMobile="300" func={() => setActivitiesModalOpened(true)}> Create new Activities </Button>

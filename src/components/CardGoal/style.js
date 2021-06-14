61519e47db436554d85ab31baee5a8fae640665e
Gustavo Silva <gustavosilvafranco643@gmail.com>
2021-06-14 14:36:32 +0000

    Feature/card goal

--- src/components/CardGoal/style.js
+++ src/components/CardGoal/style.js
@@ -19,3 +19,52 @@ export const GoalDiv = styled.div`
     }
 
 `
+
+export const ButtonsDiv = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+`
+
+export const RemoveGoalButton = styled.button`
+    width: 40%;
+    padding: 5px;
+    height: 20%;
+    color: var(--white);
+    background-color: var(--black);
+    border-radius: 5px;
+
+    @media screen and (min-width: 768px){
+        min-height: 30px;
+    }
+`
+
+export const UpdateGoalDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 40%;
+`
+
+export const UpdateGoalButton = styled(RemoveGoalButton)`
+    background-color: var(--color-primary);
+    width: 92.5%;
+    height: initial;
+`
+
+export const UpdateGoalSelectDiv = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content:space-evenly;
+    width: 92.5%;
+    select {
+        width: 50%;
+    }
+    button {
+        background-color: var(--color-primary);
+        color: var(--white);
+        border-radius: 5px;
+        margin: 2px;
+        padding: 2px;
+        width: 50%;
+    }
+`

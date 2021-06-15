3a80935990f254efc00b266d5f08aca2969a1791
Gustavo Silva <gustavosilvafranco643@gmail.com>
2021-06-15 13:28:31 +0000

    Feature/card activity

--- src/components/CardActivitie/index.jsx
+++ src/components/CardActivitie/index.jsx
@@ -1,4 +1,13 @@
-import { ActivitieDiv } from "./style";
+import { 
+    ActivitieDiv, 
+    UpdateTitleButton, 
+    TitleInput, 
+    UpdateTitleDiv, 
+    UpdateButton, 
+    RemoveActivityButton, 
+    ButtonsDiv,
+    UpdateActivityTitleDiv
+} from "./style";
 import { useState } from "react";
 import api from "../../services/api";
 
@@ -19,22 +28,38 @@ const CardActivitie = ({activitie, getGroups}) => {
         .then(response => getGroups())
         .catch(error => console.log(error))
     }
+
+    const handleRemoveActivity = (id) => {
+        api.delete(`/activities/${id}/`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+        }})
+        .then(response => getGroups())
+        .catch(error => console.log(error))
+    }
     
     return (
         <ActivitieDiv>
             <p><span>Title:</span> {activitie.title}</p>
             <p><span>Realization time:</span>{activitie.realization_time}</p>
-            <button onClick={() => setShowTitleInput(!showTitleInput)}> Update title </button>
-            {showTitleInput 
-                &&  <>
-                        <input 
-                            type="text" 
-                            onChange={e => setInputValue(e.target.value)}
-                            value={inputValue}
-                        />
-                        <button onClick={() => handleUpdateTitle(inputValue, activitie.id)}> Update </button>
-                    </>
-            }
+            <ButtonsDiv>
+                <UpdateTitleDiv>
+                    <UpdateTitleButton onClick={() => setShowTitleInput(!showTitleInput)}> Update title </UpdateTitleButton>
+                    {showTitleInput 
+                        &&  <UpdateActivityTitleDiv>
+                                <TitleInput 
+                                    type="text" 
+                                    onChange={e => setInputValue(e.target.value)}
+                                    value={inputValue}
+                                />
+                                <UpdateButton onClick={() => handleUpdateTitle(inputValue, activitie.id)}> Update </UpdateButton>
+                            </UpdateActivityTitleDiv>
+                    }
+                </UpdateTitleDiv>
+                <RemoveActivityButton onClick={() => handleRemoveActivity(activitie.id)}>
+                    Remove goal    
+                </RemoveActivityButton>
+            </ButtonsDiv>
         </ActivitieDiv>
     )
 }

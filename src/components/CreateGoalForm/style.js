dd87f63b5073458e1f22cd677b8d7b238f0fbddb
Gustavo Silva <gustavosilvafranco643@gmail.com>
2021-06-15 14:55:11 +0000

    Fix/create new goal

--- src/components/CreateGoalForm/style.js
+++ src/components/CreateGoalForm/style.js
@@ -11,3 +11,36 @@ export const NotifyP = styled.p`
     align-items: center;
     justify-content: space-evenly;
 `
+
+export const SelectDiv = styled.div`
+    display: flex;
+    align-items: center;
+    width: 550px;
+    height: 30px;
+    background-color: var(--background-gray);
+    font-size: 1.2rem;
+    height: 46px;
+    border: 1px solid var(--black);
+    border-radius: 12px;
+    box-shadow: 1px 1px var(--black);
+    margin-bottom: 20px;
+`
+
+export const IconDiv = styled.div`
+    margin: 17px;
+`
+
+export const DifficultySelect = styled.select`
+    width: 90%;
+    height: 30px;
+    border: none;
+    background-color: var(--background-gray);
+    border-radius: 5px;
+    color: var(--color-placeholder);
+    font-size: 1.2rem;
+`
+
+export const DifficultyP = styled.label`
+    width: 100%;
+    margin: 5px;
+`

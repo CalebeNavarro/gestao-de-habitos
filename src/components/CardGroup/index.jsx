f5ebda4c43b08fc69dd7b7a3a0ad56442ec88639
Michael <michael.philipe2013@gmail.com>
2021-06-16 11:18:55 -0300

    feat: styling cardGroup

--- src/components/CardGroup/index.jsx
+++ src/components/CardGroup/index.jsx
@@ -1,8 +1,27 @@
+import { useEffect, useState } from "react";
 import { Container } from "./styles";
 
 const CardGroup = ({ group, ...rest }) => {
+  const category = group.category.toLowerCase()
+
+  const [background,setBackground] = useState("")
+  const addBackgroundImg = () => {
+    if(category.includes("health")){
+      setBackground("https://ssir.org/images/articles/FullPage_Color-592.jpg")
+    }
+    if(category.includes("academia")){
+      setBackground("https://infonet.com.br/wp-content/uploads/2021/04/academia_fotofreepik_060421.jpg")
+    }
+    if(category.includes("book")){
+      setBackground("https://edsurge.imgix.net/uploads/post/image/13231/reading-1585538079.jpg?auto=compress%2Cformat&w=1024&h=512&fit=crop")
+    }
+  }
+ useEffect(()=>{
+   addBackgroundImg()
+ }, [background])
+  
   return (
-    <Container {...rest}>
+    <Container background={background} {...rest}>
       <p>Name: {group.name}</p>
 
       <p>Category: {group.category}</p>      

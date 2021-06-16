fdd5c74dbc6736c3f6a2547b1b4a35f59ddfa64d
Gustavo Silva <gustavosilvafranco643@gmail.com>
2021-06-16 13:45:07 +0000

    Feature/specific group

--- src/pages/SpecificGroup/index.jsx
+++ src/pages/SpecificGroup/index.jsx
@@ -1,16 +1,17 @@
 import Header from "../../components/Header";
 import Modal from "../../components/Modal";
-import {
-  SpecificGroupDiv,
-  SpecificGroupH3,
-  SpecificGroupP,
-  Container,
-  SpecificGroupMain,
-  ModalDiv,
-  SpecificGroupInfo,
-  UnsubscribeButton,
-  SubscribeButton,
-} from "./style";
+import { 
+    SpecificGroupDiv, 
+    SpecificGroupH3, 
+    SpecificGroupP,
+    Container,
+    SpecificGroupMain,
+    ModalDiv,
+    SpecificGroupInfo,
+    UnsubscribeButton,
+    SubscribeButton,
+    EditGroupButton
+} from "./style"
 import { useContext, useEffect, useState } from "react";
 import { useParams, Redirect, useHistory } from "react-router-dom";
 import api from "../../services/api";
@@ -33,156 +34,156 @@ const SpecificGroup = () => {
     return decoded.user_id;
   });
 
-  const [goalsDivOpened, setGoalsDivOpened] = useState(false);
-  const [activitiesDivOpened, setActivitiesDivOpened] = useState(false);
-  const [isOpened, setIsOpened] = useState(true);
-  const [goalsModalOpened, setGoalsModalOpened] = useState(false);
-  const [activitiesModalOpened, setActivitiesModalOpened] = useState(false);
-  const [subscribed, setSubscribed] = useState(false);
+    const [goalsDivOpened, setGoalsDivOpened] = useState(false);
+    const [activitiesDivOpened, setActivitiesDivOpened] = useState(false);
+    const [isOpened, setIsOpened] = useState(true);
+    const [goalsModalOpened, setGoalsModalOpened] = useState(false);
+    const [activitiesModalOpened, setActivitiesModalOpened] = useState(false);
+    const [subscribed, setSubscribed] = useState(false);
+    const [unsubscribed, setUnsubscribed] = useState(false);
+    const [creator, setCreator] = useState(false);
+    const [editModalOpened, setEditModalOpened] = useState(false);
 
-  const [groupInfo, setGroupInfo] = useState([]);
+    const [groupInfo, setGroupInfo] = useState([]);
 
-  useEffect(() => {
-    if (goalsModalOpened === true) {
-      setIsOpened(true);
-    } else if (activitiesModalOpened === true) {
-      setIsOpened(true);
-    }
-    if (isOpened === false) {
-      setGoalsModalOpened(false);
-      setActivitiesModalOpened(false);
-    }
-  }, [goalsModalOpened, activitiesModalOpened, isOpened]);
+    useEffect(() => {
+        if (goalsModalOpened === true){
+            setIsOpened(true);
+        } else if (activitiesModalOpened === true){
+            setIsOpened(true);
+        } else if (editModalOpened === true){
+            setIsOpened(true);
+        };
+        if(isOpened === false){
+            setGoalsModalOpened(false);
+            setActivitiesModalOpened(false);
+            setEditModalOpened(false);
+        };
+    }, [isOpened]);
 
-  const getGroups = () => {
-    api
-      .get(`/groups/${id}/`)
-      .then((group) => setGroupInfo(group.data))
-      .catch((error) => console.log(error));
-  };
+    const getGroups = () => {
+        api
+        .get(`/groups/${id}/`)
+        .then((group) => setGroupInfo(group.data))
+        .catch((error) => console.log(error));
+    };
 
-  useEffect(() => {
-    getGroups();
-  }, [id]);
+    useEffect(() => {
+        getGroups();
+    }, [id]);
 
-  useEffect(() => {
-    if (groupInfo.users_on_group) {
-      groupInfo.users_on_group.map((member) => {
-        if (member.id === userId) {
-          return setSubscribed(true);
-        }
-      });
-    }
-  }, [groupInfo]);
+    useEffect(() => {
+        if(groupInfo.creator){
+            if(groupInfo.creator.id === userId){
+                return setCreator(true);
+            } else if (groupInfo.users_on_group){
+                const memberType = groupInfo.users_on_group.find((member) => {
+                    return member.id === userId
+                });
+                if(memberType === undefined){
+                    setSubscribed(false);
+                    setUnsubscribed(true);
+                } else {
+                    setUnsubscribed(false);
+                    setSubscribed(true);
+                }
+            };
+        }; 
+    }, [groupInfo]);
 
-  const handleSubscribe = () => {
-    const token = JSON.parse(localStorage.getItem("@habits:token"));
-    api
-      .post(
-        `/groups/${id}/subscribe/`,
-        {},
-        {
-          headers: {
+    const handleSubscribe = () => {
+        const token = JSON.parse(localStorage.getItem("@habits:token"));
+        api.post(`/groups/${id}/subscribe/`,{}, {
+            headers: {
+                Authorization: `Bearer ${token}`
+        }})
+        .then(response => {
+            toast.info("Successful to enter!")
+            getGroups();
+            return response;
+        })
+        .catch(error => console.log(error))
+    };
+
+    const handleUnsubscribe = () => {
+        const token = JSON.parse(localStorage.getItem("@habits:token"));
+        api
+        .delete(`/groups/${id}/unsubscribe/`, {
+            headers: {
+            "Content-Type": "application/json",
             Authorization: `Bearer ${token}`,
-          },
-        }
-      )
-      .then((response) => {
-        toast.info("Successful to enter!");
-        getGroups();
-        return response;
-      })
-      .catch((error) => console.log(error));
-  };
+            },
+        })
+        .then((response) => {
+            toast.info(`Exit successful!`);
+            setSubscribed(false);
+            getGroups();
+            history.push(history.location.state.referrer);
+            return response;
+        })
+        .catch((error) => console.log(error.response));
+    };
 
-  const handleUnsubscribe = () => {
-    const token = JSON.parse(localStorage.getItem("@habits:token"));
-    api
-      .delete(`/groups/${id}/unsubscribe/`, {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      })
-      .then((response) => {
-        toast.info(`Exit successful!`);
-        setSubscribed(false);
-        getGroups();
-        history.push(history.location.state.referrer);
-        return response;
-      })
-      .catch((error) => console.log(error.response));
-  };
+    const { goals, activities } = groupInfo;
 
-  const { goals, activities } = groupInfo;
+    if (isLoged() === false) {
+        return <Redirect to="/" />;
+    }
 
-  if (isLoged() === false) {
-    return <Redirect to="/" />;
-  }
-
-  return (
-    <>
-      <Header />
-      <SpecificGroupMain>
-        <SpecificGroupDiv>
-          <SpecificGroupInfo>
-            <SpecificGroupH3>{groupInfo.name}</SpecificGroupH3>
-            <SpecificGroupP>
-              <span>Creator: </span>
-              {groupInfo.creator?.username}
-            </SpecificGroupP>
-            <SpecificGroupP>
-              <span>Category: </span>
-              {groupInfo.category}
-            </SpecificGroupP>
-            <SpecificGroupP>
-              <span>Description: </span>
-              {groupInfo.description}
-            </SpecificGroupP>
-            {subscribed ? (
-              <UnsubscribeButton onClick={handleUnsubscribe}>
-                unsubscribe
-              </UnsubscribeButton>
-            ) : (
-              <SubscribeButton onClick={handleSubscribe}>
-                subscribe
-              </SubscribeButton>
-            )}
-          </SpecificGroupInfo>
-          <Container>
-            <GoalsList
-              goalsDivOpened={goalsDivOpened}
-              setGoalsDivOpened={setGoalsDivOpened}
-              goals={goals}
-              setGoalsModalOpened={setGoalsModalOpened}
-              getGroups={getGroups}
-            />
-            <ActivitiesList
-              activitiesDivOpened={activitiesDivOpened}
-              setActivitiesDivOpened={setActivitiesDivOpened}
-              activities={activities}
-              setActivitiesModalOpened={setActivitiesModalOpened}
-              getGroups={getGroups}
-            />
-          </Container>
-        </SpecificGroupDiv>
-        <ModalDiv>
-          {goalsModalOpened && (
-            <Modal isOpened={isOpened} setIsOpened={setIsOpened}>
-              <h3>Create new Goal</h3>
-              <CreateGoalForm id={id} getGroups={getGroups} />
-            </Modal>
-          )}
-          {activitiesModalOpened && (
-            <Modal isOpened={isOpened} setIsOpened={setIsOpened}>
-              <h3>Create new Activity</h3>
-              <CreateActivityForm id={id} getGroups={getGroups} />
-            </Modal>
-          )}
-        </ModalDiv>
-      </SpecificGroupMain>
-    </>
-  );
-};
+    return (
+        <>
+            <Header />
+            <SpecificGroupMain>
+                <SpecificGroupDiv>
+                    <SpecificGroupInfo>
+                        <SpecificGroupH3>{groupInfo.name}</SpecificGroupH3>
+                        <SpecificGroupP><span>Creator: </span>{groupInfo.creator?.username}</SpecificGroupP>
+                        <SpecificGroupP><span>Category: </span>{groupInfo.category}</SpecificGroupP>
+                        <SpecificGroupP><span>Description: </span>{groupInfo.description}</SpecificGroupP>
+                        {subscribed && <UnsubscribeButton onClick={handleUnsubscribe}>Unsubscribe</UnsubscribeButton>}
+                        {unsubscribed && <SubscribeButton onClick={handleSubscribe}>Subscribe</SubscribeButton>}
+                        {creator && <EditGroupButton onClick={() => setEditModalOpened(true)}>Edit group</EditGroupButton>}
+                    </SpecificGroupInfo>
+                    <Container>
+                        <GoalsList 
+                            goalsDivOpened={goalsDivOpened}
+                            setGoalsDivOpened={setGoalsDivOpened}
+                            goals={goals}
+                            setGoalsModalOpened={setGoalsModalOpened}
+                            getGroups={getGroups}
+                        />
+                        <ActivitiesList 
+                            activitiesDivOpened={activitiesDivOpened}
+                            setActivitiesDivOpened={setActivitiesDivOpened}
+                            activities={activities}
+                            setActivitiesModalOpened={setActivitiesModalOpened}
+                            getGroups={getGroups}
+                        />
+                    </Container>
+                </SpecificGroupDiv>
+                <ModalDiv>
+                    {goalsModalOpened 
+                    &&  <Modal isOpened={isOpened} setIsOpened={setIsOpened}>
+                            <h3>Create new Goal</h3>
+                            <CreateGoalForm id={id} getGroups={getGroups} />
+                        </Modal>
+                    }
+                    {activitiesModalOpened 
+                    &&  <Modal isOpened={isOpened} setIsOpened={setIsOpened}>
+                            <h3>Create new Activitie</h3>
+                            <CreateActivityForm id={id} getGroups={getGroups} />
+                        </Modal>
+                    }
+                    {editModalOpened
+                    && <Modal isOpened={isOpened} setIsOpened={setIsOpened}>
+                            <h3>Edit group</h3>
+                            <form></form>
+                        </Modal>
+                    }
+                </ModalDiv>
+            </SpecificGroupMain>
+        </>
+    )
+}
 
 export default SpecificGroup;

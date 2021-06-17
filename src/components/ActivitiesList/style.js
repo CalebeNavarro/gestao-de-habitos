adce2c156a9a4ff18999962ee48e315475f9d16e
Gustavo Silva <gustavosilvafranco643@gmail.com>
2021-06-17 16:20:52 -0300

    style: backgrounds altereds

--- src/components/ActivitiesList/style.js
+++ src/components/ActivitiesList/style.js
@@ -9,12 +9,13 @@ export const ActivitiesDiv = styled.div`
 
 export const ActivitiesNameDiv = styled.div`
    display: none;
+   border-radius: 10px 10px 0 0;
 
     @media screen and (min-width: 768px) {
         display: flex;
         justify-content: center;
         align-items: center;
-        background-color: var(--color-primary);
+        background: radial-gradient(#1488CC,#2B32B2);
         color: var(--white);
         font-size: 3rem;
         min-width: 300px;
@@ -29,9 +30,10 @@ export const ActivitiesListDiv = styled.div`
     display: ${(props) => props.activitiesDivOpened ? "flex" : "none" };
     flex-direction: column;
     align-items: center;
-    background-color: var(--color-primary);
+    background: radial-gradient(#1488CC,#2B32B2);
     color: var(--white);
     border: 2px solid black;
+    border-radius: 0 0 10px 10px;
     border-top: none;
     max-width: 700px;
     width: 80vw;

6756e9de906d112ac8391d209c727fc7a7728c37
Michel Capela <34andromedae@gmail.com>
2021-06-17 16:43:06 +0000

    fix: membersList structure and styles

--- src/components/MembersList/index.jsx
+++ src/components/MembersList/index.jsx
@@ -1,12 +1,14 @@
 import CardMember from "../CardMember";
 import { MembersListDiv } from "./style";
 
-const MembersList = ({members}) => {
-    return (
-        <MembersListDiv>
-            {members.map((member) => <CardMember key={member.id} member={member}/>)}
-        </MembersListDiv>
-    )
-}
+const MembersList = ({ members }) => {
+  return (
+    <MembersListDiv>
+      {members.map((member) => (
+        <CardMember key={member.id} member={member} />
+      ))}
+    </MembersListDiv>
+  );
+};
 
 export default MembersList;
